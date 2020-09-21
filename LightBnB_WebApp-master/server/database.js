deb29d756fd2bc2879c50629d2a87c985ea5d07d
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-09-21 03:14:47 +0000

    updated reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -70,7 +70,22 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = {
+    text : `SELECT reservations.*,properties.*,AVG(property_reviews.rating) AS average_rating
+    FROM properties JOIN reservations
+    ON reservations.property_id = properties.id
+    JOIN property_reviews ON
+    property_reviews.property_id = properties.id
+    WHERE reservations.guest_id = $1
+    AND reservations.end_date < now()::date
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT $2`
+  }
+  return pool
+      .query(query, [guest_id,limit])
+      .then((result) => result.rows)
+      .catch((err) => err.message);
 }
 exports.getAllReservations = getAllReservations;
 
