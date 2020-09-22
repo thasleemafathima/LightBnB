8727a5d4de3029b539447b811fa0ae06917c7c2b
Thasleema Fathima Puroskhan <thasleemafathima92@gmail.com>
2020-09-22 01:29:00 +0000

    add properties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -98,12 +98,62 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  return pool.query(`
-  SELECT * FROM properties
-  LIMIT $1
-  `, [limit])
-  .then(res => res.rows);
-}
+   // 1
+   const queryParams = [];
+   // 2
+   let queryString = `
+   SELECT properties.*, avg(property_reviews.rating) as average_rating
+   FROM properties
+   JOIN property_reviews ON properties.id = property_id
+   `;
+   // 3
+   if (options.city) {
+     queryParams.push(`%${options.city.toLowerCase()}%`);
+     queryString += `WHERE LOWER(city) LIKE $${queryParams.length} `;
+   }
+   if (options.owner_id) {
+     queryParams.push(`%${options.owner_id}%`);
+     queryString += `WHERE owner_id = $${queryParams.length}`;
+   }
+   if (options.minimum_price_per_night) {
+     queryParams.push(options.minimum_price_per_night * 100);
+     if (queryParams.length === 1) {
+       queryString += `WHERE cost_per_night > $${queryParams.length}`;
+     } else {
+       queryString += `AND cost_per_night > $${queryParams.length}`;
+     }
+   }
+   if (options.maximum_price_per_night) {
+     queryParams.push(options.maximum_price_per_night * 100);
+     if (queryParams.length === 1) {
+       queryString += `WHERE cost_per_night$ < $${queryParams.length}`;
+     } else {
+       queryString += `AND cost_per_night < $${queryParams.length}`;
+     }
+   }
+   queryString += `GROUP BY properties.id`;
+   if (options.minimum_rating) {
+     queryParams.push(options.minimum_rating);
+     if (queryParams.length === 1) {
+       queryString += `
+     HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+     } else {
+       queryString += `
+     HAVING avg(property_reviews.rating) >= $${queryParams.length} `;
+     }
+   }
+   // 4
+   queryParams.push(limit);
+   queryString += `
+   ORDER BY cost_per_night
+   LIMIT $${queryParams.length};
+   `;
+   // 5
+   console.log(queryString, queryParams);
+   // 6
+   return pool.query(queryString, queryParams).then(res => res.rows);
+ };
+
 exports.getAllProperties = getAllProperties;
 
 
